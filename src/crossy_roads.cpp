1ff41d5b16bca460810e54ce48328021c8b7f811
Rachit Gupta <rachitgupta@csilvm-14.cs.ucsb.edu>
2023-11-29 00:06:02 -0800

    player object initialisation and support added

--- src/crossy_roads.cpp
+++ src/crossy_roads.cpp
@@ -1,5 +1,24 @@
 #include <GL/gl.h>
 #include <GL/glut.h>
+#include "lib/Chicken.hpp"
+
+void handleKeypress(unsigned char key, int x, int y) {
+    switch (key) {
+        case 'w': // Move up
+            player.y += 0.1f;
+            break;
+        case 's': // Move down
+            player.y -= 0.1f;
+            break;
+        case 'a': // Move left
+            player.x -= 0.1f;
+            break;
+        case 'd': // Move right
+            player.x += 0.1f;
+            break;
+    }
+    glutPostRedisplay(); // Redraw the scene
+}
 
 void update(){
 	
@@ -9,14 +28,23 @@ void render(int value){
 	glClear(GL_COLOR_BUFFER_BIT);
 	glLoadIdentity();
 
-	glColor3f(0.678, 0.847, 0.902);
-	glBegin(GL_TRIANGLES);
-		glVertex2f(0.0f, 0.5f);
-		glVertex2f(0.5f, -0.5f);
-		glVertex2f(-0.5f, -0.5f);
-	glEnd();
+	drawChicken(&player);
 
 	glFlush();
+}
+
+//Global variable
+Chicken player;
+
+void init(){
+	glClearColor(1.0, 1.0, 1.0, 0.0);
+	
+	{//initialise player
+		player.x 	 = 0.0f; // Initial X position
+    		player.y	 = 0.0f; // Initial Y position
+    		player.size	 = 0.1f; // Size of the chicken
+    		player.direction = true; // Initial Direction ~ right
+	}
 }
 
 int main(int argc, char** argv){
@@ -31,8 +59,8 @@ int main(int argc, char** argv){
 	}
 
 	glutDisplayFunc(update);
-
-	glutTimerFunc(33, render, 0); //33ms ~ 30fps
+	glutKeyboardFunc(handleKeypress);
+	glutTimerFunc(33, render, 0); 		//33ms ~ 30fps
 
 	glutMainLoop();
 
