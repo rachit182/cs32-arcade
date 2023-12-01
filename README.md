9310832ff2584410c4af60dc75eb00c68a01ba3a
Animesh Sachan <79371675+theanimated01@users.noreply.github.com>
2023-12-01 02:11:41 -0800

    CHECKPOINT - 2  (#2)
    * header file defined for chicken including the struct and draw
    
    * player object initialisation and support added
    
    * basic yellow block printing and moving
    
    * chicken ready and moving in bounds
    
    * making car body
    
    * added windows
    
    * basic code ready
    need to change coordinate system
    
    * Added Makefile, Updated README, and Implemented glLoadIdentity in DrawChicken()
    
    * road and car objects added
    
    * drew chicken last
    
    * Update README.md
    
    ---------
    
    Co-authored-by: Zeel <zpatel@ucsb.edu>
    Co-authored-by: Rachit Gupta <rachitgupta@csilvm-14.cs.ucsb.edu>
    Co-authored-by: Rachit Gupta <rachitgupta@csil-13.cs.ucsb.edu>
    Co-authored-by: Kenisha Vaswani <kenishavaswani@Kenishas-MacBook-Pro.local>
    Co-authored-by: rachit182 <rachitgupta1824@gmail.com>
    Co-authored-by: rachit182 <116847543+rachit182@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# Crossy Roads
+# Crossy Roads README
+
+Welcome to Crossy Roads! This game brings an exciting and fun experience as you guide chickens across various obstacles. Below, you will find instructions on how to compile and clean up the project, as well as an overview of the main file and library components.
+
+## Compilation and Cleanup
+
+### Compile the Game
+To compile Crossy Roads, open your command line and type the following command: 'make'
+
+### Cleanup
+If you need to clean up the compiled files, use 'make clean' on the command line.
+
+## Main File
+
+### [src/crossy_roads.cpp]
+This is the main file of the game. It integrates all the components and libraries to run Crossy Roads. It handles the game loop, event processing, and rendering.
+
+### Game Controls:
+The game is controlled using keyboard inputs. You can move the chicken using the `WASD` keys or the arrow keys (`UP`, `DOWN`, `LEFT`, `RIGHT`).
+
+
+## Library
+
+### [src/lib/...]
+
+#### Chicken.hpp
+Chicken is the main player in our game and `Chicken.hpp` contains code to define the `Chicken` structure and the `drawChicken` function. 
+This file is an essential component of the Crossy Roads game.
+
+- **Structure:** The `Chicken` struct includes properties for position (`x`, `y`) and size of the chicken. The position is updated in the main loop.
+- **Function:** The `drawChicken` function uses OpenGL commands to draw a chicken on the screen based on its position.
+
+#### car.hpp
+Car are the NPC in our game and `car.hpp` contains code to define the `car` structure and the `drawCar` function. 
+This file is also an essential component of the Crossy Roads game.
+
+- **Structure:** The `Car` struct includes properties for position (`x`, `y`), the direction the car is going, and size of the car. The position is updated in the main loop.
+- **Function:** The `drawCar` function uses OpenGL commands to draw a car on the screen based on its position (updated in main file).
+
+#### road.hpp
+Road is another object in our game on which the cars spawn and `road.hpp` contains code to define the `road` structure and the `drawRoad` function. 
+This file is also an essential component of the Crossy Roads game.
+
+- **Structure:** The `Road` struct includes properties for position (`x`, `y`), the direction the cars on this road should be going, size of the road, and the car object on the road.
+- **Function:** The `drawCar` function uses OpenGL commands to draw a road on the screen based on its position and then calls `drawCar` to draw the car over it.
--- src/crossy_roads.cpp
+++ src/crossy_roads.cpp
@@ -1,22 +1,126 @@
 #include <GL/gl.h>
 #include <GL/glut.h>
+#include "lib/Chicken.hpp"
+#include "lib/road.hpp"
 
-void update(){
-	
+//Global Variables
+Chicken player;
+float laneHeight = 0.2f;
+Road road_1(0.0f, 0.1f, laneHeight, false, laneHeight*0.8);
+Road road_2(0.0f, 0.5f, laneHeight, true, laneHeight*0.8);
+Road road_3(0.0f, -0.3f, laneHeight, true, laneHeight*0.8);
+std::vector<Road> road_list;
+
+void handleKeypress(unsigned char key, int x, int y) {
+    float movementAmount = laneHeight;
+    switch (key) {
+        case 'w': // Move up
+            if (player.y + movementAmount < 1.0f) {
+                player.y += movementAmount;
+            }
+            break;
+        case 's': // Move down
+            if (player.y - movementAmount > -1.0f) {
+                player.y -= movementAmount;
+            }
+            break;
+        case 'a': // Move left
+            if (player.x > -1.0f + laneHeight) {
+                player.x -= 0.05f;
+            }
+            player.direction = false;
+            break;
+        case 'd': // Move right
+            if (player.x < 1.0f - laneHeight){
+                player.x += 0.05f;
+            }
+            player.direction = true;
+            break;
+    }
+    glutPostRedisplay();
+    glFlush();
 }
 
-void render(int value){
+void handleSpecialKeypress(int key, int x, int y) {
+    float movementAmount = laneHeight;
+    switch (key) {
+        case GLUT_KEY_UP:
+            if (player.y + movementAmount < 1.0f) {
+                player.y += movementAmount;
+            }
+            break;
+        case GLUT_KEY_DOWN:
+            if (player.y - movementAmount > -1.0f) {
+                player.y -= movementAmount;
+            }
+            break;
+        case GLUT_KEY_LEFT:
+            if (player.x > -1.0f + laneHeight) {
+                player.x -= 0.05f;
+            }
+            player.direction = false;
+            break;
+        case GLUT_KEY_RIGHT:
+            if (player.x < 1.0f - laneHeight){
+                player.x += 0.05f;
+            }
+            player.direction = true;
+            break;
+    }
+    glutPostRedisplay();
+    glFlush();
+}
+
+void update(int value){
+	for(size_t i=0; i<road_list.size(); i++){
+		if (road_list[i].direction) {
+			if (road_list[i].car.x < 1.5f) {
+				road_list[i].car.x += 0.01f;
+			} else {
+				road_list[i].car.x = road_list[i].x - 1.2f;
+				road_list[i].car.y = road_list[i].y;
+			}
+		} else {
+			if (road_list[i].car.x > -1.5f) {
+				road_list[i].car.x -= 0.01f;
+			} else {
+				road_list[i].car.x = road_list[i].x + 1.2f;
+				road_list[i].car.y = road_list[i].y;
+			}
+		}
+	}
+    glutPostRedisplay();
+    glutTimerFunc(33, update, 0);
+}
+
+void display(){
 	glClear(GL_COLOR_BUFFER_BIT);
 	glLoadIdentity();
+  
+//C_NPC
+	for(auto road: road_list){
+		drawRoad(&road);
+	}
 
-	glColor3f(0.678, 0.847, 0.902);
-	glBegin(GL_TRIANGLES);
-		glVertex2f(0.0f, 0.5f);
-		glVertex2f(0.5f, -0.5f);
-		glVertex2f(-0.5f, -0.5f);
-	glEnd();
+//B_Player
+	drawChicken(&player);
 
-	glFlush();
+	glFlush(); //single buffering
+}
+
+void init(){
+	glClearColor(0.133, 0.545, 0.133, 0.0);
+  
+	{//initialise player
+		player.x 	 =  0.0f; 		// Initial X position
+    		player.y	 = -1.0f + 0.5*laneHeight; 		// Initial Y position
+    		player.size	 =  0.8*laneHeight;	// Size of the chicken
+    		player.direction =  true; 		// Initial Direction ~ right
+	}
+  
+	road_list.push_back(road_1);
+	road_list.push_back(road_2);
+	road_list.push_back(road_3);
 }
 
 int main(int argc, char** argv){
@@ -27,15 +131,14 @@ int main(int argc, char** argv){
 		glutInitWindowPosition(100,100);
 		glutCreateWindow("Crossy Roads");
 
-		glClearColor(1.0, 1.0, 1.0, 0.0);
+		init();
 	}
-
-	glutDisplayFunc(update);
-
-	glutTimerFunc(33, render, 0); //33ms ~ 30fps
-
+	
+	glutKeyboardFunc(handleKeypress);
+	glutSpecialFunc(handleSpecialKeypress);
+	glutDisplayFunc(display);
+	glutTimerFunc(33, update, 0);
 	glutMainLoop();
 
 	return 0;
 }
-
